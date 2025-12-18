377272123b38e6e1a257edb5ec6ad6744f0f9a57
euan-griffiths <griffithseuan01@icloud.com>
2025-12-18 10:53:12 +0000

    Added Error Handling

--- Main_Code/Image_Procsesing.py
+++ Main_Code/Image_Procsesing.py
@@ -2,36 +2,38 @@ from astropy.io import fits
 from matplotlib import pyplot as plt
 import numpy as np
 
+
 class Main():
     def __init__(self):
         self.imageLink = r"FITS_IMAGES\ESA\502nmos.fits" #
         self.cmap = "CMRmap"
         self.lowerbound = 0
         self.upperbound = 70
+        self.location = 0
     
     def main(self):
-        try:
-            with fits.open(self.imageLink) as self.hdul:
-                self.data = self.hdul[0].data
-                self.header = self.hdul[0].header
-        except FileNotFoundError:
-            
         
-        plt.imshow(self.data,cmap=self.cmap,vmin=self.lowerbound,vmax=self.upperbound)
-        plt.colorbar()
-        self.render()
+        try:
+            with fits.open(self.imageLink) as self.hdul: # Opens the image and gets the image Data for rendering and the Header for credits 
+                self.data = self.hdul[self.location].data
+                self.header = self.hdul[self.location].header
+                
+        except FileNotFoundError: #File Erroring handeling
+            return ("Incorrect File Path")
+        except:
+            return("Unkown Error")
+        
+        try: # Image rendering and error detection
+            plt.imshow(self.data,cmap=self.cmap,vmin=self.lowerbound,vmax=self.upperbound)
+            plt.colorbar()
+            self.render()
+        except:
+            return("Rendering Error")
         
             
     def render(self):
         plt.show()
-        try:
-            while True:
-                pass
-        except KeyboardInterrupt:
-            return
         
-    
-    
     
 if __name__ == "__main__":
     main = Main()
