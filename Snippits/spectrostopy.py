0ef8fd7c9c7c23214921ffabb6d5997b554189d2
euan-griffiths <griffithseuan01@icloud.com>
2025-12-29 16:01:01 +0000

    sorting

--- Snippits/spectrostopy.py
+++ Snippits/spectrostopy.py
@@ -1,37 +1,59 @@
 
 class spectrostcopy():
     def __init__(self):
-        self.array = [["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"],
-                      ["nan",1,2,3,4,5,6,7,8,9,"nan"]]
+        self.array = [[ 5,  0,  3,  3,  7,  9,  3, 'nan' ,  2,  4,  7,  6,],
+                      [ 8,  8,  1,  6,  7,  7,  8,  1,  5,  9,  8,  9,],
+                      [ 4,  3,  0, 'nan', 'nan',  0,  2, 'nan',  8,  1,  3, 'nan'],
+                      ['nan',  7,  0,  1,  9, 'nan',  0,  4,  7,  3, 'nan',  7,],
+                      [ 2,  0,  0,  4, 'nan', 'nan',  6,  8,  4, 'nan',  4, 'nan'],
+                      [ 8,  1,  1,  7,  9,  9,  3,  6,  7,  2,  0,  3,],
+                      [ 5,  9,  4,  4, 'nan',  4,  4,  3,  4,  4,  8,  4,],
+                      [ 3,  7,  5,  5,  0,  1,  5,  9, 'nan', 'nan',  5,  0,],
+                      [ 1,  2,  4,  2,  0,  3,  2,  0,  7,  5,  9,  0,],
+                      ['nan',  7,  2,  9,  2,  3,  3, 'nan',  3,  4,  1,  2,],
+                      [ 9,  1,  4,  6,  8,  2,  3,  0,  0,  6,  0,  6,],
+                      [ 3,  3,  8,  8,  8,  2,  3,  2,  0,  8,  8,  3,]]
+
         self.total_data = 0
         self.data = []
 
     def main(self):
         for a2ndlarray in self.array:
+            
             values = list(filter(lambda a: a!= "nan", a2ndlarray))
-            print(values)
+            
             self.total_data += len(values)
+            
             while len(values) != 0:
                 initlen = len(values)
                 target = values[0]
+                
                 values = list(filter(lambda a:a!= target,values))
+                
                 endlen = len(values)
                 sublen = initlen-endlen
+                
                 targetfound = False
                 for pos,dat in enumerate(self.data):
                     if dat[0] == target:
                         targetfound = True
-
-                self.data.append([target,sublen])
+                        break
                     
+                if targetfound == False:
+                    self.data.append([target,sublen])
+                    
+                if targetfound == True:
+                    sublen += self.data[pos][1]
+                    self.data.pop(pos)
+                    self.data.append([target,sublen])
+                    
+        print(self.data)
+        self.sortdata()
         
+          
+    def sortdata(self):
+        self.data.sort(key=lambda a:a[0])
+        print(self.data)
             
             
 if __name__ =="__main__":
