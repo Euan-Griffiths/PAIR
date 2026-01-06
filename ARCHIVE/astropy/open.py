0b63af0db4ddaddaa87a4d3e203d6ad40c189c3f
euan-griffiths <griffithseuan01@icloud.com>
2026-01-06 09:01:09 +0000

    Ineficeint

--- ARCHIVE/astropy/open.py
+++ ARCHIVE/astropy/open.py
@@ -1,6 +1,7 @@
 from astropy.io import fits
 from matplotlib import pyplot as plt
 import numpy
+from tqdm import tqdm
 
 hdul = fits.open(r"502nmos.fits")
 info = hdul[0].header
@@ -16,3 +17,61 @@ plt.colorbar()
 plt.show()
 input()
 hdul.close()
+
+# --------------------
+
+
+class spectrostcopy():
+    def __init__(self):
+        hdul = fits.open(r"502nmos.fits")
+        self.arrays = hdul[0].data
+        hdul.close()
+        self.total_data = 0
+        self.data = []
+
+    def main(self):
+        for array in tqdm(self.arrays):
+            
+            values = list(filter(lambda a: a!= "nan", array)) # Removes all nan values from the data
+            
+            self.total_data += len(values) # gets total amount of data to caculate a precentage, for the amount of wavelength emmisions at that value
+            
+            while len(values) != 0:
+                initlen = len(values) # gets current length of the array of values
+                target = values[0] # gets current first value of the array to check the rest of the array
+                
+                values = list(filter(lambda a:a!= target,values)) # Removes all instances of the target from the array
+                
+                endlen = len(values) 
+                sublen = initlen-endlen # finds how much data has been removed from the array, giving the frequancy of the removed value
+                
+                targetfound = False
+                for pos,dat in enumerate(tqdm(self.data)): #Checks if the value being added to the data array is already in the array
+                    if dat[0] == target:
+                        targetfound = True
+                        break
+                    
+                if targetfound == False:
+                    self.data.append([target,sublen]) # adds the target and frequany into the data  array
+                    
+                if targetfound == True: # increase the sum of the frequancy of the target when it alreadys exists in the array
+                    sublen += self.data[pos][1]
+                    self.data.pop(pos)
+                    self.data.append([target,sublen])
+        self.percentages()
+        print(self.data) 
+        self.sortdata()
+    
+    def percentages(self): # turns the data in to percentages for the graph
+        print(self.total_data)
+        for i,dat in enumerate(self.data):
+            self.data[i][1] = round(dat[1]/self.total_data*100)
+          
+    def sortdata(self):
+        self.data.sort(key=lambda a:a[0]) # sorts the data array based on the target value
+        print(self.data)
+            
+            
+if __name__ =="__main__":
+    spec = spectrostcopy()
+    spec.main()
